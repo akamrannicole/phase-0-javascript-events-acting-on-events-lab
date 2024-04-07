2a7261472fac057481dc994cbee78c3bc088d193
user.name <akamrannicole@gmail.com>
2024-04-07 11:54:42 +0300

    JS events acting lab done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Your code here
+// Define moveDodgerLeft function
+function moveDodgerLeft() {
+    const left = dodger.style.left.replace('px', '');
+    const leftNum = parseInt(left, 10);
+
+    if (leftNum > 0) {
+        dodger.style.left = `${leftNum - 4}px`;
+    }
+}
+
+// Define moveDodgerRight function
+function moveDodgerRight() {
+    const left = dodger.style.left.replace('px', '');
+    const leftNum = parseInt(left, 10);
+
+    if (leftNum < 360) {
+        dodger.style.left = `${leftNum + 4}px`;
+    }
+}
